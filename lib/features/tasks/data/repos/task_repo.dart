4d407fa4766e7b48b1524a81d3718a9c8f953773
firas <firashedfi4@gmail.com>
2025-08-05 14:26:07 +0100

    feat(tasks): added either and failure class

--- lib/features/tasks/data/repos/task_repo.dart
+++ lib/features/tasks/data/repos/task_repo.dart
@@ -1,13 +1,24 @@
+import 'package:dartz/dartz.dart';
+import 'package:task_management_app/core/errors/failure.dart';
 import 'package:task_management_app/features/tasks/data/models/task_model.dart';
 
 abstract class TaskRepo {
-  // Future<List<TaskModel>> fetchTasks(String userId); //* GET /tasks
-  Future<TaskModel> getTaskById(int id, String userId); //* / GET /tasks/:id
-  Future<TaskModel> createTask(TaskModel task, String userId); //* POST /tasks
-  Future<TaskModel> updateTask(
+  // Future<Either<Failure, List<TaskModel>>> fetchTasks(String userId); //* GET /tasks
+  Future<Either<Failure, TaskModel>> getTaskById(
+    int id,
+    String userId,
+  ); //* / GET /tasks/:id
+  Future<Either<Failure, TaskModel>> createTask(
+    TaskModel task,
+    String userId,
+  ); //* POST /tasks
+  Future<Either<Failure, TaskModel>> updateTask(
     int id,
     TaskModel task,
     String userId,
   ); //* PATCH /tasks/:id
-  Future<void> deleteTask(int id, String userId); //* DELETE /tasks/:id
+  Future<Either<Failure, void>> deleteTask(
+    int id,
+    String userId,
+  ); //* DELETE /tasks/:id
 }
