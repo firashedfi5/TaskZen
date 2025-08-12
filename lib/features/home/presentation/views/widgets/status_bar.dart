9578e9535bd509fa911b7e9ec79a6b27e7c8cc8f
firas <firashedfi4@gmail.com>
2025-08-12 13:41:23 +0100

    feat(status-bar): convert StatusBar to StatefulWidget and implement dynamic filter chips

--- lib/features/home/presentation/views/widgets/status_bar.dart
+++ lib/features/home/presentation/views/widgets/status_bar.dart
@@ -1,51 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:task_management_app/core/constants.dart';
 
-class StatusBar extends StatelessWidget {
+class StatusBar extends StatefulWidget {
   const StatusBar({super.key});
+
+  @override
+  State<StatusBar> createState() => _StatusBarState();
+}
+
+class _StatusBarState extends State<StatusBar> {
+  String? selectedItem;
+
+  final List<String> statusOptions = ['To Do', 'Progress', 'Done', 'Cancelled'];
 
   @override
   Widget build(BuildContext context) {
     return SliverPadding(
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
       sliver: SliverToBoxAdapter(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            FilterChip(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              side: const BorderSide(color: Colors.black),
-              label: const Text('To Do'),
-              onSelected: (value) {},
-            ),
-            FilterChip(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              side: const BorderSide(color: Colors.black),
-              label: const Text('Progress'),
-              onSelected: (value) {},
-            ),
-            FilterChip(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              side: const BorderSide(color: Colors.black),
-              label: const Text('Done'),
-              onSelected: (value) {},
-            ),
-            FilterChip(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              side: const BorderSide(color: Colors.black),
-              label: const Text('Cancelled'),
-              onSelected: (value) {},
-            ),
-          ],
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: statusOptions.map((status) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6),
+                child: buildFilterChip(status),
+              );
+            }).toList(),
+          ),
         ),
       ),
+    );
+  }
+
+  Widget buildFilterChip(String status) {
+    final bool isSelected = selectedItem == status;
+
+    return FilterChip(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      side: BorderSide(color: isSelected ? kSecondaryColor : Colors.black),
+      label: Text(
+        status,
+        style: isSelected
+            ? const TextStyle(color: Colors.white, fontWeight: FontWeight.bold)
+            : null,
+      ),
+      showCheckmark: false,
+      selected: isSelected,
+      onSelected: (value) {
+        setState(() {
+          selectedItem = value ? status : null;
+        });
+      },
+      selectedColor: kSecondaryColor,
     );
   }
 }
